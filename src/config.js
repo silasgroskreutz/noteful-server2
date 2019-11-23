1ce46a02436e4e5b7dabd07f4179a54ff688e474
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-23 10:33:24 -0600

    second attempt

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'development',
+  DATABASE_URL: process.env.DATABASE_URL ||  'postgres://qlglribfgfcobj:043def52324e3e8afed03378c95bf5bc377f6c7de5963e259eca0d91b463c879@ec2-54-227-249-202.compute-1.amazonaws.com:5432/d1tdgqbiv9h2ao',
 };
 
